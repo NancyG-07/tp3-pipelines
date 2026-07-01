5c54c26a20e6e1814f4e1eaa8b96b13d8d7a7d23
NancyG-07 <2683295@csfoy.ca>
2026-07-01 05:35:16 -0400

    On enlève la cédule.

--- main.py
+++ main.py
@@ -3,11 +3,40 @@ import pipeline.getdataService
 import pipeline.savedataService
 
 
+import argparse
+import time
 
 
 def main():
-    data= pipeline.getdataService.fetch_data(limit=200);
-    pipeline.savedataService.saveData(data);
+
+
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument("--schedule", action="store_true")
+    parser.add_argument("--interval", type=float, default=1)
+
+    args = parser.parse_args()
+
+    if args.schedule:
+        print(f"Mode planifié aux {args.interval} heure(s)")
+
+        while True:
+            print("Exécution du pipeline...")
+            data= pipeline.getdataService.fetch_data(limit=200);
+            pipeline.savedataService.saveData(data);           
+
+            time.sleep(args.interval * 3600)
+
+    else:
+        print("Exécution unique")
+        # run_pipeline()
+
+
+
+
+
+
+
 
 
 if __name__ == "__main__":
